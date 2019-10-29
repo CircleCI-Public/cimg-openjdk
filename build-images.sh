7af44cc2118404942ad9f789341070c7e40b4463
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-10-29 17:50:41 -0400

    Add OpenJDK v13.0.1. (#18)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 13.0/Dockerfile -t cimg/openjdk:13.0.0  -t cimg/openjdk:13.0 .
+docker build --file 13.0/Dockerfile -t cimg/openjdk:13.0.1  -t cimg/openjdk:13.0 .
