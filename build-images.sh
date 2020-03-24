3738a68b335144157f0b8e881b6f3900dba58579
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-23 20:58:55 -0400

    Respin OpenJDK v8 build 242 to enable node variant. [release] (#44)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 14.0/Dockerfile -t cimg/openjdk:14.0.0  -t cimg/openjdk:14.0 .
+docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.242  -t cimg/openjdk:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.242-node  -t cimg/openjdk:8.0-node .
