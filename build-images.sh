ab59450f7314c35edbd57aaa027f27c82ab846cb
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-23 21:59:09 -0400

    Respin OpenJDK v13.0.2 to enable node variant. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.6  -t cimg/openjdk:11.0 .
-docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.6-node  -t cimg/openjdk:11.0-node .
+docker build --file 13.0/Dockerfile -t cimg/openjdk:13.0.2  -t cimg/openjdk:13.0 .
+docker build --file 13.0/node/Dockerfile -t cimg/openjdk:13.0.2-node  -t cimg/openjdk:13.0-node .
