c2a3900b9ad359326d1ce5693edbcf230dacf71c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-04-15 18:50:53 -0400

    Publish OpenJDK v11.0.7. [release] (#48)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 14.0/Dockerfile -t cimg/openjdk:14.0.0  -t cimg/openjdk:14.0 .
-docker build --file 14.0/node/Dockerfile -t cimg/openjdk:14.0.0-node  -t cimg/openjdk:14.0-node .
+docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.7  -t cimg/openjdk:11.0 .
+docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.7-node  -t cimg/openjdk:11.0-node .
