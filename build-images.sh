d92472db67d8b95ecf8277ede826d19759fa2f78
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-04-17 15:57:08 -0400

    Release OpenJDK v8.0.252. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.7  -t cimg/openjdk:11.0 .
-docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.7-node  -t cimg/openjdk:11.0-node .
+docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.252  -t cimg/openjdk:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.252-node  -t cimg/openjdk:8.0-node .
