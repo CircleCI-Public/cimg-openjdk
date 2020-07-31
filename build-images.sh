2e50c51f41759e6642b29021bf2b5166052683da
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-07-31 12:08:02 -0400

    Publish v11.0.8 and v14.0.2. [release] (#56)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.262  -t cimg/openjdk:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.262-node  -t cimg/openjdk:8.0-node .
+docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.8  -t cimg/openjdk:11.0 .
+docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.8-node  -t cimg/openjdk:11.0-node .
+docker build --file 14.0/Dockerfile -t cimg/openjdk:14.0.2  -t cimg/openjdk:14.0 .
+docker build --file 14.0/node/Dockerfile -t cimg/openjdk:14.0.2-node  -t cimg/openjdk:14.0-node .
