df5c6c7cc54632e589eb471944b5a922f294f9b3
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-10-19 13:40:53 -0400

    Add browser variant.

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 15.0/Dockerfile -t cimg/openjdk:15.0.0  -t cimg/openjdk:15.0 .
-docker build --file 15.0/node/Dockerfile -t cimg/openjdk:15.0.0-node  -t cimg/openjdk:15.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.265-browsers  -t cimg/openjdk:8.0-browsers .
+docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.8-browsers  -t cimg/openjdk:11.0-browsers .
+docker build --file 14.0/browsers/Dockerfile -t cimg/openjdk:14.0.2-browsers  -t cimg/openjdk:14.0-browsers .
+docker build --file 15.0/browsers/Dockerfile -t cimg/openjdk:15.0.0-browsers  -t cimg/openjdk:15.0-browsers .
