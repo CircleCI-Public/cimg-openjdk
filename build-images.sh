38aed77fcc7a57ec6be7bf1428c40aa7158ff944
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-18 22:25:49 -0500

    Respin browsers tags for xvfb. [release] (#73)

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.272  -t cimg/openjdk:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.272-node  -t cimg/openjdk:8.0-node .
-docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.272-browsers  -t cimg/openjdk:8.0-browsers .
-docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.9  -t cimg/openjdk:11.0 .
-docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.9-node  -t cimg/openjdk:11.0-node .
-docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.9-browsers  -t cimg/openjdk:11.0-browsers .
-docker build --file 15.0/Dockerfile -t cimg/openjdk:15.0.1  -t cimg/openjdk:15.0 .
-docker build --file 15.0/node/Dockerfile -t cimg/openjdk:15.0.1-node  -t cimg/openjdk:15.0-node .
-docker build --file 15.0/browsers/Dockerfile -t cimg/openjdk:15.0.1-browsers  -t cimg/openjdk:15.0-browsers .
+docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.265-browsers  -t cimg/openjdk:8.0-browsers .
+docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.8-browsers  -t cimg/openjdk:11.0-browsers .
+docker build --file 14.0/browsers/Dockerfile -t cimg/openjdk:14.0.2-browsers  -t cimg/openjdk:14.0-browsers .
+docker build --file 15.0/browsers/Dockerfile -t cimg/openjdk:15.0.0-browsers  -t cimg/openjdk:15.0-browsers .
