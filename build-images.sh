24a5157ab2d4f770b4b7f2419e13c2b58d90c8c1
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-04-05 13:42:16 +0100

    Java LTS (Long Term Support) versions
    For a few use cases having all LTS versions would be useful. Build on 1.8 and test on 11. I'm aware of some projects currently using 1.8 images and then installing 11 themselves which seams a waste.

--- build-images.sh
+++ build-images.sh
@@ -9,3 +9,6 @@ docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.12-browsers  -
 docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.0  -t cimg/openjdk:17.0 .
 docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.0-node  -t cimg/openjdk:17.0-node .
 docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.0-browsers  -t cimg/openjdk:17.0-browsers .
+docker build --file lts/Dockerfile -t cimg/openjdk:lts-8.0.302-11.0.12  -t cimg/openjdk:lts-8-11 .
+docker build --file lts/node/Dockerfile -t cimg/openjdk:lts-8.0.302-11.0.12-node  -t cimg/openjdk:lts-8-11-node .
+docker build --file lts/browsers/Dockerfile -t cimg/openjdk:lts-8.0.302-11.0.12-browsers  -t cimg/openjdk:8-11-browsers .
