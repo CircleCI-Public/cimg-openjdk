33008e6d87f85266862b591a521bde4ed2b81e27
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-09-10 19:55:41 +0100

    Java 8.0.292 as people still needing it

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.292  -t cimg/openjdk:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.292-node  -t cimg/openjdk:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.292-browsers  -t cimg/openjdk:8.0-browsers .
 docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.12  -t cimg/openjdk:11.0 .
 docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.12-node  -t cimg/openjdk:11.0-node .
 docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.12-browsers  -t cimg/openjdk:11.0-browsers .
