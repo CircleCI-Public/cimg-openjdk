e9f2e6847bd07edd91e176f3b272dcc6e7f031e7
HennaAbbas <henna@circleci.com>
2021-09-24 15:24:21 -0600

    Pub: 12.0.0,13.0.0,14.0.0,15.0.0, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.12  -t cimg/openjdk:11.0 .
-docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.12-node  -t cimg/openjdk:11.0-node .
-docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.12-browsers  -t cimg/openjdk:11.0-browsers .
-docker build --file 16.0/Dockerfile -t cimg/openjdk:16.0.2  -t cimg/openjdk:16.0 .
-docker build --file 16.0/node/Dockerfile -t cimg/openjdk:16.0.2-node  -t cimg/openjdk:16.0-node .
-docker build --file 16.0/browsers/Dockerfile -t cimg/openjdk:16.0.2-browsers  -t cimg/openjdk:16.0-browsers .
+docker build --file 12.0/Dockerfile -t cimg/openjdk:12.0.0  -t cimg/openjdk:12.0 .
+docker build --file 12.0/node/Dockerfile -t cimg/openjdk:12.0.0-node  -t cimg/openjdk:12.0-node .
+docker build --file 12.0/browsers/Dockerfile -t cimg/openjdk:12.0.0-browsers  -t cimg/openjdk:12.0-browsers .
+docker build --file 13.0/Dockerfile -t cimg/openjdk:13.0.0  -t cimg/openjdk:13.0 .
+docker build --file 13.0/node/Dockerfile -t cimg/openjdk:13.0.0-node  -t cimg/openjdk:13.0-node .
+docker build --file 13.0/browsers/Dockerfile -t cimg/openjdk:13.0.0-browsers  -t cimg/openjdk:13.0-browsers .
+docker build --file 14.0/Dockerfile -t cimg/openjdk:14.0.0  -t cimg/openjdk:14.0 .
+docker build --file 14.0/node/Dockerfile -t cimg/openjdk:14.0.0-node  -t cimg/openjdk:14.0-node .
+docker build --file 14.0/browsers/Dockerfile -t cimg/openjdk:14.0.0-browsers  -t cimg/openjdk:14.0-browsers .
+docker build --file 15.0/Dockerfile -t cimg/openjdk:15.0.0  -t cimg/openjdk:15.0 .
+docker build --file 15.0/node/Dockerfile -t cimg/openjdk:15.0.0-node  -t cimg/openjdk:15.0-node .
+docker build --file 15.0/browsers/Dockerfile -t cimg/openjdk:15.0.0-browsers  -t cimg/openjdk:15.0-browsers .
+docker build --file 16.0/Dockerfile -t cimg/openjdk:16.0.0  -t cimg/openjdk:16.0 .
+docker build --file 16.0/node/Dockerfile -t cimg/openjdk:16.0.0-node  -t cimg/openjdk:16.0-node .
+docker build --file 16.0/browsers/Dockerfile -t cimg/openjdk:16.0.0-browsers  -t cimg/openjdk:16.0-browsers .
