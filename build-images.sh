66e6a1dfd985108eea43989b39cec5d053f90466
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-12-13 10:06:02 -0500

    Publish v8.0.312 and v11.0.13. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.1  -t cimg/openjdk:17.0 .
-docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.1-node  -t cimg/openjdk:17.0-node .
-docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.1-browsers  -t cimg/openjdk:17.0-browsers .
+docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.312  -t cimg/openjdk:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.312-node  -t cimg/openjdk:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.312-browsers  -t cimg/openjdk:8.0-browsers .
+docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.13  -t cimg/openjdk:11.0 .
+docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.13-node  -t cimg/openjdk:11.0-node .
+docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.13-browsers  -t cimg/openjdk:11.0-browsers .
