88998eae7e68ff8408479e6d75f17030dab619a2
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-12-20 19:17:30 -0500

    Respin LTSs and v16 for log4j security fix [release] (#109)

--- build-images.sh
+++ build-images.sh
@@ -6,3 +6,9 @@ docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.312-browsers  -t
 docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.13  -t cimg/openjdk:11.0 .
 docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.13-node  -t cimg/openjdk:11.0-node .
 docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.13-browsers  -t cimg/openjdk:11.0-browsers .
+docker build --file 16.0/Dockerfile -t cimg/openjdk:16.0.2  -t cimg/openjdk:16.0 .
+docker build --file 16.0/node/Dockerfile -t cimg/openjdk:16.0.2-node  -t cimg/openjdk:16.0-node .
+docker build --file 16.0/browsers/Dockerfile -t cimg/openjdk:16.0.2-browsers  -t cimg/openjdk:16.0-browsers .
+docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.1  -t cimg/openjdk:17.0 .
+docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.1-node  -t cimg/openjdk:17.0-node .
+docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.1-browsers  -t cimg/openjdk:17.0-browsers .
