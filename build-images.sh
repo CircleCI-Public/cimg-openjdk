ff0ec23a258dc52d12869c5668f1763c156cf517
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-01-31 15:47:39 -0500

    Publish v8.0.322. [release] (#122)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.2  -t cimg/openjdk:17.0 .
-docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.2-node  -t cimg/openjdk:17.0-node .
-docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.2-browsers  -t cimg/openjdk:17.0-browsers .
+docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.322  -t cimg/openjdk:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.322-node  -t cimg/openjdk:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.322-browsers  -t cimg/openjdk:8.0-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/openjdk:17.0.2
-docker push cimg/openjdk:17.0
-docker push cimg/openjdk:17.0.2-node
-docker push cimg/openjdk:17.0-node
-docker push cimg/openjdk:17.0.2-browsers
-docker push cimg/openjdk:17.0-browsers
+docker push cimg/openjdk:8.0.322
+docker push cimg/openjdk:8.0
+docker push cimg/openjdk:8.0.322-node
+docker push cimg/openjdk:8.0-node
+docker push cimg/openjdk:8.0.322-browsers
+docker push cimg/openjdk:8.0-browsers
