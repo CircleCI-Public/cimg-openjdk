780898f798e247a9dba0cb5bd7f87a801a98e45f
Jeff Chen <jeff.chen@circleci.com>
2022-04-22 09:44:01 -0700

    Publish v17.0.3. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/openjdk:8.0.322
-docker push cimg/openjdk:8.0
-docker push cimg/openjdk:8.0.322-node
-docker push cimg/openjdk:8.0-node
-docker push cimg/openjdk:8.0.322-browsers
-docker push cimg/openjdk:8.0-browsers
+docker push cimg/openjdk:17.0.3
+docker push cimg/openjdk:17.0
+docker push cimg/openjdk:17.0.3-node
+docker push cimg/openjdk:17.0-node
+docker push cimg/openjdk:17.0.3-browsers
+docker push cimg/openjdk:17.0-browsers
