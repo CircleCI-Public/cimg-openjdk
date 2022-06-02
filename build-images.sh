9c71c9f53ef6e0ab0cbf6b85015ddd497868a4dd
Jeff Chen <jeff.chen@circleci.com>
2022-06-02 11:26:42 -0700

    [release] but removing openjdk 17 from build and push images

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.3  -t cimg/openjdk:17.0 .
-docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.3-node  -t cimg/openjdk:17.0-node .
-docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.3-browsers  -t cimg/openjdk:17.0-browsers .
 docker build --file 18.0/Dockerfile -t cimg/openjdk:18.0.1 -t cimg/openjdk:18.0 .
 docker build --file 18.0/node/Dockerfile -t cimg/openjdk:18.0.1-node  -t cimg/openjdk:18.0-node .
 docker build --file 18.0/browsers/Dockerfile -t cimg/openjdk:18.0.1-browsers  -t cimg/openjdk:18.0-browsers .
