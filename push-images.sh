0622571c08b3c51e69abb45c0eab8235af15a44b
Ruben Mennes <mennes.rubne@gmail.com>
2022-06-02 21:39:05 +0200

    Add java 18 image (#126)
    * Add java 18 image
    
    * [release] but removing openjdk 17 from build and push images
    
    Co-authored-by: Ruben Mennes <ruben.mennes@aaqua.live>
    Co-authored-by: Jeff Chen <jeff.chen@circleci.com>

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/openjdk:17.0.3
-docker push cimg/openjdk:17.0
-docker push cimg/openjdk:17.0.3-node
-docker push cimg/openjdk:17.0-node
-docker push cimg/openjdk:17.0.3-browsers
-docker push cimg/openjdk:17.0-browsers
+docker push cimg/openjdk:18.0.1
+docker push cimg/openjdk:18.0
+docker push cimg/openjdk:18.0.1-node
+docker push cimg/openjdk:18.0-node
+docker push cimg/openjdk:18.0.1-browsers
+docker push cimg/openjdk:18.0-browsers
