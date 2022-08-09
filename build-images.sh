f47153959dfb33ad094d65dd30778c27ccebd8ab
Jalexchen <jeff.chen@circleci.com>
2022-08-09 10:50:00 -0700

    Publish v17.0.4 and v18.0.2. [release] (#128)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 18.0/Dockerfile -t cimg/openjdk:18.0.1  -t cimg/openjdk:18.0 .
-docker build --file 18.0/node/Dockerfile -t cimg/openjdk:18.0.1-node  -t cimg/openjdk:18.0-node .
-docker build --file 18.0/browsers/Dockerfile -t cimg/openjdk:18.0.1-browsers  -t cimg/openjdk:18.0-browsers .
+docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.4  -t cimg/openjdk:17.0 .
+docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.4-node  -t cimg/openjdk:17.0-node .
+docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.4-browsers  -t cimg/openjdk:17.0-browsers .
+docker build --file 18.0/Dockerfile -t cimg/openjdk:18.0.2  -t cimg/openjdk:18.0 .
+docker build --file 18.0/node/Dockerfile -t cimg/openjdk:18.0.2-node  -t cimg/openjdk:18.0-node .
+docker build --file 18.0/browsers/Dockerfile -t cimg/openjdk:18.0.2-browsers  -t cimg/openjdk:18.0-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/openjdk:18.0.1
+docker push cimg/openjdk:17.0.4
+docker push cimg/openjdk:17.0
+docker push cimg/openjdk:17.0.4-node
+docker push cimg/openjdk:17.0-node
+docker push cimg/openjdk:17.0.4-browsers
+docker push cimg/openjdk:17.0-browsers
+
+docker push cimg/openjdk:18.0.2
 docker push cimg/openjdk:18.0
-docker push cimg/openjdk:18.0.1-node
+docker push cimg/openjdk:18.0.2-node
 docker push cimg/openjdk:18.0-node
-docker push cimg/openjdk:18.0.1-browsers
+docker push cimg/openjdk:18.0.2-browsers
 docker push cimg/openjdk:18.0-browsers
