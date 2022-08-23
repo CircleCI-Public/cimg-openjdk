0bd4dbbf0da60622e097ee0740028a605531de93
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-08-23 10:32:59 -0400

    Publish v8.0.345 and v11.0.16. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.4  -t cimg/openjdk:17.0 .
-docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.4-node  -t cimg/openjdk:17.0-node .
-docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.4-browsers  -t cimg/openjdk:17.0-browsers .
-docker build --file 18.0/Dockerfile -t cimg/openjdk:18.0.2  -t cimg/openjdk:18.0 .
-docker build --file 18.0/node/Dockerfile -t cimg/openjdk:18.0.2-node  -t cimg/openjdk:18.0-node .
-docker build --file 18.0/browsers/Dockerfile -t cimg/openjdk:18.0.2-browsers  -t cimg/openjdk:18.0-browsers .
+docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.345  -t cimg/openjdk:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.345-node  -t cimg/openjdk:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.345-browsers  -t cimg/openjdk:8.0-browsers .
+docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.16  -t cimg/openjdk:11.0 .
+docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.16-node  -t cimg/openjdk:11.0-node .
+docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.16-browsers  -t cimg/openjdk:11.0-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/openjdk:17.0.4
-docker push cimg/openjdk:17.0
-docker push cimg/openjdk:17.0.4-node
-docker push cimg/openjdk:17.0-node
-docker push cimg/openjdk:17.0.4-browsers
-docker push cimg/openjdk:17.0-browsers
+docker push cimg/openjdk:8.0.345
+docker push cimg/openjdk:8.0
+docker push cimg/openjdk:8.0.345-node
+docker push cimg/openjdk:8.0-node
+docker push cimg/openjdk:8.0.345-browsers
+docker push cimg/openjdk:8.0-browsers
 
-docker push cimg/openjdk:18.0.2
-docker push cimg/openjdk:18.0
-docker push cimg/openjdk:18.0.2-node
-docker push cimg/openjdk:18.0-node
-docker push cimg/openjdk:18.0.2-browsers
-docker push cimg/openjdk:18.0-browsers
+docker push cimg/openjdk:11.0.16
+docker push cimg/openjdk:11.0
+docker push cimg/openjdk:11.0.16-node
+docker push cimg/openjdk:11.0-node
+docker push cimg/openjdk:11.0.16-browsers
+docker push cimg/openjdk:11.0-browsers
