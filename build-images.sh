1d3703011b5d9db72bfdbbb49c41c4becb0a9714
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-27 13:41:07 -0400

    Publish v19.0.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 19.0/Dockerfile -t cimg/openjdk:19.0.0 -t cimg/openjdk:19.0 .
-docker build --file 19.0/node/Dockerfile -t cimg/openjdk:19.0.0-node -t cimg/openjdk:19.0-node .
-docker build --file 19.0/browsers/Dockerfile -t cimg/openjdk:19.0.0-browsers -t cimg/openjdk:19.0-browsers .
+docker build --file 19.0/Dockerfile -t cimg/openjdk:19.0.1 -t cimg/openjdk:19.0 .
+docker build --file 19.0/node/Dockerfile -t cimg/openjdk:19.0.1-node -t cimg/openjdk:19.0-node .
+docker build --file 19.0/browsers/Dockerfile -t cimg/openjdk:19.0.1-browsers -t cimg/openjdk:19.0-browsers .
