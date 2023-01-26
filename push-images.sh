6f3dbfc4daabfa763fbba0760c697a11717335b8
Jeff Chen <jeff.chen@circleci.com>
2023-01-26 10:05:39 -0800

    Pub: v8.0.362, v11.0.18, v17.0.6, and more. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/openjdk:8.0
+docker push cimg/openjdk:8.0.362
+docker push cimg/openjdk:8.0-node
+docker push cimg/openjdk:8.0.362-node
+docker push cimg/openjdk:8.0-browsers
+docker push cimg/openjdk:8.0.362-browsers
+docker push cimg/openjdk:11.0
+docker push cimg/openjdk:11.0.18
+docker push cimg/openjdk:11.0-node
+docker push cimg/openjdk:11.0.18-node
+docker push cimg/openjdk:11.0-browsers
+docker push cimg/openjdk:11.0.18-browsers
+docker push cimg/openjdk:17.0
+docker push cimg/openjdk:17.0.6
+docker push cimg/openjdk:17.0-node
+docker push cimg/openjdk:17.0.6-node
+docker push cimg/openjdk:17.0-browsers
+docker push cimg/openjdk:17.0.6-browsers
 docker push cimg/openjdk:19.0
-docker push cimg/openjdk:19.0.1
+docker push cimg/openjdk:19.0.2
 docker push cimg/openjdk:19.0-node
-docker push cimg/openjdk:19.0.1-node
+docker push cimg/openjdk:19.0.2-node
 docker push cimg/openjdk:19.0-browsers
-docker push cimg/openjdk:19.0.1-browsers
+docker push cimg/openjdk:19.0.2-browsers
