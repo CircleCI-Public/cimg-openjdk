e190acc896c0402abb33689e8e1e1d025a8b6ea2
Jalexchen <jeff.chen@circleci.com>
2023-01-27 14:10:11 -0800

    Pub: v8.0.362, v11.0.18, v17.0.6, and more. [release] (#145)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 19.0/Dockerfile -t cimg/openjdk:19.0.1 -t cimg/openjdk:19.0 .
-docker build --file 19.0/node/Dockerfile -t cimg/openjdk:19.0.1-node -t cimg/openjdk:19.0-node .
-docker build --file 19.0/browsers/Dockerfile -t cimg/openjdk:19.0.1-browsers -t cimg/openjdk:19.0-browsers .
+docker build --file 8.0/Dockerfile -t cimg/openjdk:8.0.362 -t cimg/openjdk:8.0 --platform linux/amd64 .
+docker build --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.362-node -t cimg/openjdk:8.0-node --platform linux/amd64 .
+docker build --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.362-browsers -t cimg/openjdk:8.0-browsers --platform linux/amd64 .
+docker build --file 11.0/Dockerfile -t cimg/openjdk:11.0.18 -t cimg/openjdk:11.0 --platform linux/amd64 .
+docker build --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.18-node -t cimg/openjdk:11.0-node --platform linux/amd64 .
+docker build --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.18-browsers -t cimg/openjdk:11.0-browsers --platform linux/amd64 .
+docker build --file 17.0/Dockerfile -t cimg/openjdk:17.0.6 -t cimg/openjdk:17.0 --platform linux/amd64 .
+docker build --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.6-node -t cimg/openjdk:17.0-node --platform linux/amd64 .
+docker build --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.6-browsers -t cimg/openjdk:17.0-browsers --platform linux/amd64 .
+docker build --file 19.0/Dockerfile -t cimg/openjdk:19.0.2 -t cimg/openjdk:19.0 --platform linux/amd64 .
+docker build --file 19.0/node/Dockerfile -t cimg/openjdk:19.0.2-node -t cimg/openjdk:19.0-node --platform linux/amd64 .
+docker build --file 19.0/browsers/Dockerfile -t cimg/openjdk:19.0.2-browsers -t cimg/openjdk:19.0-browsers --platform linux/amd64 .
