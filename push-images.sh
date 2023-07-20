128c137e240f7b15d398b632c30743cb113d4087
Adam Hartley <git@ahartley.com>
2023-07-20 19:54:17 +0100

    Build with buildx

--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/openjdk:11.0
-docker push cimg/openjdk:11.0.20
-docker push cimg/openjdk:11.0-node
-docker push cimg/openjdk:11.0.20-node
-docker push cimg/openjdk:11.0-browsers
-docker push cimg/openjdk:11.0.20-browsers
-docker push cimg/openjdk:17.0
-docker push cimg/openjdk:17.0.8
-docker push cimg/openjdk:17.0-node
-docker push cimg/openjdk:17.0.8-node
-docker push cimg/openjdk:17.0-browsers
-docker push cimg/openjdk:17.0.8-browsers
+set -eo pipefail
