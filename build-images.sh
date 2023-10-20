e9f5d61671490c35baee450d6bc99be2b68fcb76
Jeff Chen <jeff.chen@circleci.com>
2023-10-20 11:58:07 -0700

    Publish v11.0.21 and v17.0.9. [release]

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,9 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 21.0/Dockerfile -t cimg/openjdk:21.0.0 -t cimg/openjdk:21.0 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 21.0/node/Dockerfile -t cimg/openjdk:21.0.0-node -t cimg/openjdk:21.0-node --push .
-docker buildx build --platform=linux/amd64 --file 21.0/browsers/Dockerfile -t cimg/openjdk:21.0.0-browsers -t cimg/openjdk:21.0-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 11.0/Dockerfile -t cimg/openjdk:11.0.21 -t cimg/openjdk:11.0 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.21-node -t cimg/openjdk:11.0-node --push .
+docker buildx build --platform=linux/amd64 --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.21-browsers -t cimg/openjdk:11.0-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 17.0/Dockerfile -t cimg/openjdk:17.0.9 -t cimg/openjdk:17.0 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 17.0/node/Dockerfile -t cimg/openjdk:17.0.9-node -t cimg/openjdk:17.0-node --push .
+docker buildx build --platform=linux/amd64 --file 17.0/browsers/Dockerfile -t cimg/openjdk:17.0.9-browsers -t cimg/openjdk:17.0-browsers --push .
