339dcd81674ae26c82b251c294c43f7057e3cdb9
Bryan Johnson <bryan@circleci.com>
2024-10-15 09:20:43 -0400

    Publish v-m, v22, and v23. [release]

--- build-images-temp.sh
+++
@@ -1,3 +0,0 @@
-docker buildx build --platform=linux/amd64,linux/arm64 --file 22.0/Dockerfile -t cimg/openjdk:22.0.2 -t cimg/openjdk:22.0 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 22.0/node/Dockerfile -t cimg/openjdk:22.0.2-node -t cimg/openjdk:22.0-node --push .
-docker buildx build --platform=linux/amd64 --file 22.0/browsers/Dockerfile -t cimg/openjdk:22.0.2-browsers -t cimg/openjdk:22.0-browsers --push .
