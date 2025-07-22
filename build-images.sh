d730bf3ab500b13f85b08af41934a5d12f1fc285
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-07-22 16:43:43 -0500

    Temporarily remove java 8

--- build-images.sh
+++ build-images.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 8.0/Dockerfile -t cimg/openjdk:8.0.462 -t cimg/openjdk:8.0 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.462-node -t cimg/openjdk:8.0-node --push .
-docker buildx build --platform=linux/amd64 --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.462-browsers -t cimg/openjdk:8.0-browsers --push .
+# docker buildx build --platform=linux/amd64,linux/arm64 --file 8.0/Dockerfile -t cimg/openjdk:8.0.462 -t cimg/openjdk:8.0 --push .
+# docker buildx build --platform=linux/amd64,linux/arm64 --file 8.0/node/Dockerfile -t cimg/openjdk:8.0.462-node -t cimg/openjdk:8.0-node --push .
+# docker buildx build --platform=linux/amd64 --file 8.0/browsers/Dockerfile -t cimg/openjdk:8.0.462-browsers -t cimg/openjdk:8.0-browsers --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file 11.0/Dockerfile -t cimg/openjdk:11.0.28 -t cimg/openjdk:11.0 --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file 11.0/node/Dockerfile -t cimg/openjdk:11.0.28-node -t cimg/openjdk:11.0-node --push .
 docker buildx build --platform=linux/amd64 --file 11.0/browsers/Dockerfile -t cimg/openjdk:11.0.28-browsers -t cimg/openjdk:11.0-browsers --push .
